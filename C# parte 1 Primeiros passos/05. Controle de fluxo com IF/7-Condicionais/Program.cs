ce3a7a1516905fd78cf138c490b45b2b337762c7
Naran Gomes <gom3snaran@gmail.com>
2021-12-08 18:13:50 -0300

    Aula sobre controle de fluxo com IF

---
+++ C# parte 1 Primeiros passos/05. Controle de fluxo com IF/7-Condicionais/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 7-Condicionais");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+
+            if (idadeJoao >= 18)
+            {
+                Console.WriteLine("É maior de idade. Pode entrar.");
+            }
+            else
+            {
+                if(quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("É menor de idade, mas está acompanhado. Pode entrar.");
+                }
+                else
+                {
+                    Console.WriteLine("É menor de idade. Não pode entrar.");
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
