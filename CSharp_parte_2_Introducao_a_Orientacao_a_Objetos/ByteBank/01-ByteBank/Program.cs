fc28b25ed95999c26a0a9352a06e76731ab00c19
Naran Gomes <gom3snaran@gmail.com>
2021-12-11 17:11:10 -0300

    Mudanca no nome de pastas e adicao de exercicios de POO

---
+++ CSharp_parte_2_Introducao_a_Orientacao_a_Objetos/ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 862;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine("Agência: " + contaDaGabriela.agencia);
+            Console.WriteLine("Número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+            Console.WriteLine();
+
+            ContaCorrente primeiraContaCorrente = new ContaCorrente();
+            primeiraContaCorrente.saldo = 200;
+
+            Console.WriteLine(primeiraContaCorrente.saldo);
+
+            primeiraContaCorrente.saldo += 100;
+            Console.WriteLine(primeiraContaCorrente.saldo);
+
+            ContaCorrente segundaContaCorrente = new ContaCorrente();
+            segundaContaCorrente.saldo = 50;
+
+            Console.WriteLine("primeira conta tem : " + primeiraContaCorrente.saldo);
+            Console.WriteLine("segunda conta tem : " + segundaContaCorrente.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp_parte_2_Introducao_a_Orientacao_a_Objetos/ByteBank/02-ByteBank/ContaCorrente.cs
@@ -0,0 +1,7 @@
+﻿    public class ContaCorrente
+    {
+        public string titular { get; set; }
+        public int agencia { get; set; }
+        public int numero { get; set; }
+        public double saldo = 100;
+    }
