b73aa64a0d94fd20cee88dfd2282072980768e23
Naran Gomes <gom3snaran@gmail.com>
2021-12-13 14:06:59 -0300

    Exercícios sobre metodos e funcoes em classes

---
+++ CSharp_parte_2_Introducao_a_Orientacao_a_Objetos/ByteBank/03-ByteBank/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoNaran = new ContaCorrente();
+            contaDoNaran.titular = "Naran";
+            contaDoNaran.Depositar(2200);
+
+            Console.WriteLine(contaDoNaran.saldo);
+
+            ContaCorrente contaDaSamile = new ContaCorrente();
+            contaDaSamile.titular = "Samile";
+            Console.WriteLine(contaDaSamile.saldo);
+
+            contaDoNaran.Transferir(500, contaDaSamile);
+            Console.WriteLine(contaDoNaran.saldo);
+            Console.WriteLine(contaDaSamile.saldo);
+
+
+
+        }
+    }
+}
