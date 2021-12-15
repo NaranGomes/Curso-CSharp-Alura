af862f632fe4a79b1d6a9fcd149675bb47880850
Naran Gomes <gom3snaran@gmail.com>
2021-12-14 23:07:03 -0300

    Correcao encapsulamento atributo e propriedade saldo 06ByteBank

--- CSharp_parte_2_Introducao_a_Orientacao_a_Objetos/ByteBank/06-ByteBank/ContaCorrente.cs
+++ CSharp_parte_2_Introducao_a_Orientacao_a_Objetos/ByteBank/06-ByteBank/ContaCorrente.cs
@@ -11,33 +11,48 @@ namespace _06_ByteBank
         public Cliente Titular { get; set; }
         public int Agencia { get; set; }
         public int Numero { get; set; }
-        public double Saldo { get; set; }
 
-       
+        private double _saldo = 100;
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
+
         public void Depositar(double valor)
         {
-            Saldo += valor;
+            _saldo += valor;
         }
 
         public bool Sacar(double valor)
         {
-            if (Saldo < valor)
+            if (_saldo < valor)
             {
                 return false;
             }
 
-            Saldo -= valor;
+            _saldo -= valor;
             return true;
         }
 
         public bool Transferir(double valor, ContaCorrente ContaDestino)
         {
-            if (Saldo < valor)
+            if (_saldo < valor)
             {
                 return false;
             }
 
-            Saldo -= valor;
+            _saldo -= valor;
             ContaDestino.Depositar(valor);
             return true;
         }
