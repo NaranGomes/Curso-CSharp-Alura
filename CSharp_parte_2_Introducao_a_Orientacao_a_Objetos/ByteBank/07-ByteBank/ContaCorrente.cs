13fd98c3b2a0c97e2d37496355cc3199eb67d5e7
Naran Gomes <gom3snaran@gmail.com>
2021-12-15 14:37:35 -0300

    Construtores recebendo argumentos e membros estaticos

---
+++ CSharp_parte_2_Introducao_a_Orientacao_a_Objetos/ByteBank/07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    internal class ContaCorrente
+    {
+        public static int TotaldeContasCriadas { get; private set; }
+        public Cliente Titular { get; set; }
+
+        public int _agencia;
+        public int Agencia
+        { 
+            get 
+            { 
+                return _agencia;
+            }
+            set
+            {
+                if (value <=0)
+                {
+                    return;
+                }
+                _agencia = value;
+            }
+        }
+        public int Numero { get; set; }
+
+        private double _saldo = 100;
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+
+            TotaldeContasCriadas++;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            return true;
+        }
+
+        public bool Transferir(double valor, ContaCorrente ContaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            ContaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
