8ea9b041ae0d5cba74f7f80db80fd66d79b9e5c8
Naran Gomes <gom3snaran@gmail.com>
2021-12-31 15:17:49 -0300

    Referenciar DLLs sem depender do código fonte do projeto

---
+++ Csharpe_parte_5_Bibliotecas_DLLs_Documentação_e_usando_NuGet/ByteBank.SistemaInterno/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,18 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaInterno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(458, 458909);
+            Console.WriteLine(conta.Saldo);
+        }
+    }
+}
