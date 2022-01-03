ffce9b4c4f1e6b6ecf99bfca7f912a2b76919e0c
Naran Gomes <gom3snaran@gmail.com>
2022-01-03 11:32:41 -0300

    Metodo Substring

---
+++ Csharpe_parte_6_Strings_ExpressoesRegulares_e_a_classe_Object/Strings/Manipulacao_strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manipulacao_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "pagina?argumentos";
+            string argumentos = url.Substring(7);
+            Console.WriteLine(argumentos);
+        }
+    }
+}
