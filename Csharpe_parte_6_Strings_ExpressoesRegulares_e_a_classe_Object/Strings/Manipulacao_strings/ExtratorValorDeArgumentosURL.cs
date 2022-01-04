b103eb12585099c9c359f099aeb0a5abb476ed3d
Naran Gomes <gom3snaran@gmail.com>
2022-01-04 10:09:26 -0300

    Métodos ToUpper, ToLower, StartsWith, EndsWith, Contains, Replace e Remove

--- Csharpe_parte_6_Strings_ExpressoesRegulares_e_a_classe_Object/Strings/Manipulacao_strings/ExtratorValorDeArgumentosURL.cs
+++ Csharpe_parte_6_Strings_ExpressoesRegulares_e_a_classe_Object/Strings/Manipulacao_strings/ExtratorValorDeArgumentosURL.cs
@@ -21,5 +21,23 @@ namespace Manipulacao_strings
             int indiceInterrogacao = url.IndexOf('?');
             _argumentos = url.Substring( indiceInterrogacao + 1);
         }
+
+        public string GetValor(string nomeParametro)
+        {
+            string argumentosEmCaixaAlta = _argumentos.ToUpper();
+            nomeParametro = nomeParametro.ToUpper();
+
+            string termo = nomeParametro + "=";
+            int indiceTermo = argumentosEmCaixaAlta.IndexOf(termo);
+            
+            string resultado = _argumentos.Substring( indiceTermo + termo.Length);
+
+            int indexEComercial = resultado.IndexOf('&');
+            if (indexEComercial == -1)
+            {
+                return resultado;
+            }
+            return resultado.Remove(indexEComercial);
+        }
     }
 }
--- Csharpe_parte_6_Strings_ExpressoesRegulares_e_a_classe_Object/Strings/Manipulacao_strings/Program.cs
+++ Csharpe_parte_6_Strings_ExpressoesRegulares_e_a_classe_Object/Strings/Manipulacao_strings/Program.cs
@@ -10,11 +10,27 @@ namespace Manipulacao_strings
     {
         static void Main(string[] args)
         {
+            string urlTeste = "http://www.bytebank.com/cambio";
+            int indiceByteBank = urlTeste.IndexOf("http://www.bytebank.com");
+
+            Console.WriteLine(urlTeste.StartsWith("http://www.bytebank.com")); //true
+            Console.WriteLine(urlTeste.EndsWith("/cambio")); //true
+            Console.WriteLine(urlTeste.EndsWith("cambio/")); //False
+
+            Console.WriteLine(urlTeste.Contains("ByteBank")); //False devido ao case sensitive
+
             string url = "pagina?argumentos";
             int indiceInterrogacao = url.IndexOf('?');
             
             string argumentos = url.Substring(indiceInterrogacao + 1);
             Console.WriteLine(argumentos);
+
+            string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
+            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL(urlParametros);
+
+            Console.WriteLine("Moeda de Origem: " + extrator.GetValor("MoEdAOrIgEm"));
+            Console.WriteLine("Moeda de Destino: " + extrator.GetValor("mOeDaDeStInO"));
+            Console.WriteLine(extrator.GetValor("VaLor"));
 
 
         }
