7dae244bea304f3ad0a690a710cac36338051d68
Naran Gomes <gom3snaran@gmail.com>
2022-01-04 14:52:17 -0300

    Regex, Match e IsMatch

--- Csharpe_parte_6_Strings_ExpressoesRegulares_e_a_classe_Object/Strings/Manipulacao_strings/Program.cs
+++ Csharpe_parte_6_Strings_ExpressoesRegulares_e_a_classe_Object/Strings/Manipulacao_strings/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Manipulacao_strings
@@ -10,6 +11,33 @@ namespace Manipulacao_strings
     {
         static void Main(string[] args)
         {
+            // Olá, meu nome é Guilherme e você pode entrar em contato comigo
+            // através do número 8457-4456!
+
+            // Meu nome é Guilherme, me ligue em 4784-4546
+
+            //string padrao ="[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+            //string padrao ="[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            //string padrao = "[0-9]{4,5}[-][0-9]{4}";
+            //string padrao = "[0-9]{4,5}[-]{0,1}[0-9]{4}";
+            //string padrao = "[0-9]{4,5}-{0,1}[0-9]{4}";
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
+            
+            string textoDeTeste = "Meu nome é Guilherme, me ligue em 4784-4546";
+            string textoDeTeste2 = "Meu nome é Guilherme, me ligue em 981844546";
+
+            //Console.WriteLine(Regex.IsMatch(textoDeTeste, padrao));
+            Match resultado = Regex.Match(textoDeTeste, padrao);
+            Console.WriteLine(resultado.Value);
+
+            resultado = Regex.Match(textoDeTeste2, padrao);
+
+            //Console.WriteLine(resultado); Dá no mesmo!
+            Console.WriteLine(resultado.Value);
+
+
+            Console.ReadLine();
+
             string urlTeste = "http://www.bytebank.com/cambio";
             int indiceByteBank = urlTeste.IndexOf("http://www.bytebank.com");
 
