0d027e90d06b8bfbe62b3e38df69c9b64bc75326
Naran Gomes <gom3snaran@gmail.com>
2022-01-07 16:31:33 -0300

    Criando classes Genericas

--- Csharp_parte_7_Array_e_tipos_genericos/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ Csharp_parte_7_Array_e_tipos_genericos/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -13,10 +13,35 @@ namespace ByteBank.SistemaAgencia
         static void Main(string[] args)
         {
 
+            ListaDeObject listaDeIdades = new ListaDeObject();
+
+            listaDeIdades.AdicionarVarios(23, 12, 54);
+            listaDeIdades.AdicionarVarios(23, 12, "Deu ruim!");
+
+            for (int i = 0; i < listaDeIdades.Tamanho; i++)
+            {
+                int idade = (int)listaDeIdades[i];
+                Console.WriteLine(idade);
+
+            }
+
+            Console.ReadLine();
+
             Console.WriteLine(SomarVarios(1, 2, 3));
             Console.WriteLine(SomarVarios(1, 2, 3, 5, 6));
 
-            
+            Lista<int> idades = new Lista<int>();
+
+            idades.AdicionarVarios(63, 15, 21, 50);
+            idades.Remover(15);
+
+            Lista<string> cursos = new Lista<string>();
+            cursos.AdicionarVarios("C# Parte 1", "C# Parte 2", "C# Parte 3");
+
+            Lista<ContaCorrente> contas = new Lista<ContaCorrente>();
+            contas.AdicionarVarios(new ContaCorrente(124, 54354), new ContaCorrente(201, 44354));
+
+
 
             Console.ReadLine();
 
